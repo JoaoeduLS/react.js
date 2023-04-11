46d0911a5ec208f0b1e4db23c3f6d97ce7a07eeb
unknown <21114290013@ACADEMICO.IESB.BR>
2023-04-11 20:51:39 -0300

    Api axios services

--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-const apiFilmes = axios.create();
+const apiFilmes = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTZmNjNlZjZlZGQ5N2E4ODgyZDlkNmM4MWZiMGI5MyIsInN1YiI6IjY0MzVlMDM4YWVkZTU5MDBmNDA4NzE2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AkgWWXNBHCTHykw7GG2XiVFiRIFoN6jbxpW6aMt6xMs",
+  },
+});
 
 export default apiFilmes;
